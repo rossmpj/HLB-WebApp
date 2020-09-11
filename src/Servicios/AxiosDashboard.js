d2d9b8c475db77766724c560e60ec3a58186011d
Rosa Pinccay <rmpincay@espol.edu.ec>
2020-09-11 12:38:41 -0500

    Error en dashboard resuelto

--- src/Servicios/AxiosDashboard.js
+++ src/Servicios/AxiosDashboard.js
@@ -7,23 +7,23 @@ export default class AxiosDashboard {
 
 
   static obtener_numero_total_equipos = () => {
-    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_equipos/`);
+    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_equipos`);
   }
 
   static obtener_numero_total_ips = () =>{
-    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_ips/`);
+    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_ips`);
   }
 
   static obtener_numero_total_marcas = () =>{
-    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_marcas/`);
+    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_marcas`);
   }
 
   static obtener_numero_total_programas = () =>{
-    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_programas/`);
+    return AxiosDashboard.instanceAxios.get(`/obtener_numero_total_programas`);
   }
 
   static mostrar_solicitudes = () =>{
-    return AxiosDashboard.instanceAxios.get(`/mostrar_solicitudes_dashboard/`);
+    return AxiosDashboard.instanceAxios.get(`/mostrar_solicitudes_dashboard`);
   }
 
 }
